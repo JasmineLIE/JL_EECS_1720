cf7e1329b184d47612e4090b2260b166648aaaab
Jasmine Ly <73720805+JasmineLIE@users.noreply.github.com>
2022-02-16 12:51:02 -0500

    Python brrrrrr

--- Practice Work/Python/Beginner Python Work/In VSCode/OpeningAndReadingTextFiles/OpeningReadingWritingBinFiles.py
+++ Practice Work/Python/Beginner Python Work/In VSCode/OpeningAndReadingTextFiles/OpeningReadingWritingBinFiles.py
@@ -2,3 +2,13 @@
 #To work with ibnaru files, use 'rb' or 'wb' mode
 #copy a jpeg into your desktop and rename it myImage.jpg, now edit the program above by changing the first two lines
 
+inputFile = open ('myImage.jpg', 'rb')
+outputFile = open('myOutputImage.jpg', 'wb')
+
+msg = inputFile.read(10)
+
+while len(msg):
+    outputFile.write(msg)
+    msg = inputFile.read(10)
+inputFile.close()
+outputFile.close()
---
+++ Practice Work/Python/Beginner Python Work/In VSCode/OpeningAndReadingTextFiles/deletingRenamingFiles.py
@@ -0,0 +1,12 @@
+'''
+Two other useful functions are remove() and rename() functions
+
+These are avaiable in the os module and need to be imported
+
+remove() -- function deletes a file.  The syntax is remove(filename)
+e.x: remove('myfile.txt)
+
+rename() -- renames a file.  The syntax is (old name, new name)
+e.x: rename('oldfile.txt', 'newfile.txt')
+
+'''
