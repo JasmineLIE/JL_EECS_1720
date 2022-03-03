1022f39a66b76ed215d4dac28dafd91ea342890a
Jasmine Ly <jasminely428@gmail.com>
2022-03-03 00:10:34 -0500

    More Server Practice

--- Practice Work/Python/Sockets/client.py
+++ Practice Work/Python/Sockets/client.py
@@ -2,16 +2,34 @@
 Much like server, uses the same socket, but instead of binding we connect
 '''
 
+
 import socket #already comes installed
+
+HEADERSIZE = 10
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 2000)) #can communicate to and from Python programs on the same machine, on local network set of machines, or remote network
 
 while True:
-    msg = s.recv(8) #rhis is the buffer; TCP socket is a stream of data.  Have to decide how big of chunks of data do we want to receive at a time.  IN this case 1024
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message length: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
 
-    print(msg.decode("utf-8")) #communicating via byte-stream.  Receive bytes, read bytes then decode bytes
+        full_msg += msg.decode("utf=8")
+
+        if len(full_msg) -HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = '' #empty it out
 
 '''
+
 Now run on command prompt
 '''
--- Practice Work/Python/Sockets/server.py
+++ Practice Work/Python/Sockets/server.py
@@ -1,16 +1,28 @@
 import socket #already comes installed
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #declaring the family type, which is AF_INET -- SOCK_STREAM is a streaming socket
 s.bind((socket.gethostname(), 2000)) #lower digits already occupied by some other program
 s.listen(5) #prepare server to listen and leave queue of 5 when messages start stacking up
 
+HEADERSIZE = 10
+
 while True:
     clientsocket, address = s.accept()
     #anybody connects and then we store client socket object into the clientsocket and address where they're coming from
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8")) #sending information and what type of bytes
+    msg = "Welcome to the Server!"
+    msg = f'{len(msg):<{HEADERSIZE}}' +msg #the longest message length we can receive is 10.  The buffer will always be more than enough to handle the header size
+    #the < in <(HEADERSIZE) is the alignment -- it's going to be left aligned when it appears in the console
+    clientsocket.send(bytes(msg, "utf-8")) #sending information and what type of bytes
     #send information to the clientsocket
-
+    
+    while True: 
+        time.sleep(3)
+        msg = f"The time is! {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}' +msg 
+        clientsocket.send(bytes(msg, "utf-8"))
+        #every three seconds, the message is sent that checks the time
 '''
 What is a Socket?
 
